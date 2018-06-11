045b159a5db04ebb8c77ed72dbe53f77e85ffc95
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-11 19:41:15 +0800

    Create __init__.py

---
+++ Pointnity Network-file/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+	# -*- coding: utf-8 -*-
+	"""
+	Pointnity Network-file
+	~~~~~
+	copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+	copyright: (c) 2016 by Pointnity Network.org
+	
+	This file is part of Pointnity Network-file.
+	
+	Pointnity Network-file is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Pointnity Network-file is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with Pointnity Network-file. If not, see <http://www.gnu.org/licenses/>.
+	"""
+	
+	from Pointnity Network_file import file_key_regenerate, file_put, file_get, file_delete, file_list, file_sign, file_verify, main
+	from Pointnity Network_file import CONFIG_PATH
