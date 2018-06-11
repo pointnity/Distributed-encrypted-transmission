481cb92d067643cd9aa0e8ac0aec5df9edecefa0
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-11 19:11:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,3 +15,12 @@ setup(
     author_email='support@blockstack.org',
     description='Blockstack encrypted file sharing',
     keywords='blockchain git crypography name key value store data',
+    packages=find_packages(),
+    download_url='https://github.com/blockstack/blockstack-file/archive/master.zip',
+    zip_safe=False,
+    include_package_data=True,
+    scripts=['bin/blockstack-file'],
+    install_requires=[
+        'blockstack>=0.14.1',
+        'blockstack-gpg>=0.14.1'
+    ],
