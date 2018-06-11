d5495a9eb4b2323f809424d9b2ca5ee656d9e68b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-11 19:11:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,3 +5,13 @@ from setuptools import setup, find_packages
 
 # to set __version__
 exec(open('blockstack_file/version.py').read())
+
+setup(
+    name='blockstack-file',
+    version=__version__,
+    url='https://github.com/blockstack/blockstack-file',
+    license='GPLv3',
+    author='Blockstack.org',
+    author_email='support@blockstack.org',
+    description='Blockstack encrypted file sharing',
+    keywords='blockchain git crypography name key value store data',
